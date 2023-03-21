cbdeb24a3b62cec421dd9dd9dfb9b840bac54c26
Mathys Cogne Foucault <mathys.cognef@gmail.com>
2023-03-21 13:03:03 +0100

    Récupérer dynamiquement les données des travaux via l’API

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -11,7 +11,7 @@ function fetchProjets() {
     fetch('http://localhost:5678/api/works') // Récupère les données de l'API
         .then(response => response.json()) // Transforme la réponse en JSON
         .then(data => {
-            dataProjets(data)
+            generationProjets(data)
         })
         .catch(error => console.error(error)); // Affiche les erreurs dans la console 
 }
@@ -22,7 +22,43 @@ function fetchProjets() {
 
 
 
+///////////////////////////////////////////////////////////////////////
+////////////// GENERARTION ////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+const sectionProjets = document.querySelector(".gallery"); 
 
-function dataProjets(data) {
-    console.log(data)
+
+
+
+///////////////////////////////////////////////////////////////////////
+function resetSectionProjets() {  
+	sectionProjets.innerHTML = "";
 }
+
+function generationProjets(data) {
+    console.log(data)
+    resetSectionProjets()
+
+    for (let i = 0; i < data.length; i++) {
+        const figure = document.createElement("figure");
+        sectionProjets.appendChild(figure);
+
+        const img = document.createElement("img");
+        img.src = data[i].imageUrl;
+        img.alt = data[i].title;
+        figure.appendChild(img);
+
+        const figcaption = document.createElement("figcaption");
+        figcaption.innerHTML = data[i].title;
+        figure.appendChild(figcaption);
+    }
+}
+
+
+// <section id="portfolio">
+// <h2>Mes Projets</h2>
+// <div class="gallery">
+//     <figure>
+//         <img src="assets/images/abajour-tahina.png" alt="Abajour Tahina">
+//         <figcaption>Abajour Tahina</figcaption>
+//     </figure>
