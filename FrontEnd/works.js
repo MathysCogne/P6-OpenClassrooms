12535339bb0f38313da51fff5f60fab19d473c6e
Mathys Cogne Foucault <mathys.cognef@gmail.com>
2023-03-21 12:44:49 +0100

    cancel import export

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,3 +1,28 @@
-// import arrayProjets from "./init.js";
-import {dataProjets} from "./init.js";
-// console.log(arrayProjets + "test");
+///////////////////////////////////////////////////////////////////////
+////////////// RECUPERATION DES DONEES DEPUIS l'API ///////////////////
+///////////////////////////////////////////////////////////////////////
+fetchProjets()
+
+///////////////////////////////////////////////////////////////////////
+// DONNES SECTION PROJETS 
+
+// let arrayProjets = {}; 
+function fetchProjets() { 
+    fetch('http://localhost:5678/api/works') // Récupère les données de l'API
+        .then(response => response.json()) // Transforme la réponse en JSON
+        .then(data => {
+            dataProjets(data)
+        })
+        .catch(error => console.error(error)); // Affiche les erreurs dans la console 
+}
+
+///////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+function dataProjets(data) {
+    console.log(data)
+}
