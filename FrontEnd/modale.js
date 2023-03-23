5c1b9a1106399844fcfa5ca04596b0d1dcfc1ce6
Mathys Cogne Foucault <mathys.cognef@gmail.com>
2023-03-23 01:14:44 +0100

    JS func test token / afficher admin modale

--- FrontEnd/modale.js
+++ FrontEnd/modale.js
@@ -1,3 +1,7 @@
+////////////////////////////////////////////////////
+////////////// GESTION BOITE MODALE ////////////////
+////////////////////////////////////////////////////
+
 let modale = null;
 
 // Ouverture de la modale
@@ -18,22 +22,21 @@ const openModale = function(e) {
 
 //  Ferme la modale
 const closeModale = function(e) {
+    e.preventDefault()
     if (modale === null) return
 
-    e.preventDefault()
-    const target = document.querySelector(e.target.getAttribute("href"))
+    
+    modale.setAttribute("aria-hidden", "true")
+    modale.removeAttribute("aria-modal")
+
+    modale.querySelector(".js-modale-close").removeEventListener("click", closeModale)
+    modale.querySelector(".js-modale-stop").removeEventListener("click", stopPropagation)
 
     // Fermeture de la modale apres 500ms 
     window.setTimeout(function() {
         modale.style.display = "none"
         modale = null
     }, 400)
-    
-    modale.setAttribute("aria-hidden", "true")
-    modale.removeAttribute("aria-modal")
-
-    modale.removeEventListener("click", closeModale).removeEventListener("click", closeModale)
-    modale.querySelector(".js-modale-stop").removeEventListener("click", stopPropagation)
 };
 
 
@@ -53,3 +56,30 @@ window.addEventListener("keydown", function(e) {
         closeModale(e)
     }
 });
+
+
+////////////////////////////////////////////////////
+////////////// GESTION TOKEN LOGIN /////////////////
+////////////////////////////////////////////////////
+
+// Récupération du token
+const token = localStorage.getItem("token");
+// const test = document.querySelectorAll(".admin__modifer");
+adminPanel()
+
+function adminPanel() {
+    document.querySelectorAll(".admin__modifer").forEach(a => {
+        console.log(a)
+        if (token === null) {
+            return;
+        }
+        else {
+            a.removeAttribute("aria-hidden")
+            a.removeAttribute("style")
+            console.log("teszgdiqdiqzbdihzduizqdhu")
+        }
+    });
+}
+    
+// Si le token existe, on affiche le bouton de déconnexion
+
